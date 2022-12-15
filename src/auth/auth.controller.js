bba87716193f0bceeda222e892caa53725ca3597
Lili <102866531+lilux91@users.noreply.github.com>
2022-12-14 23:18:58 -0300

    primercommit

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,21 @@
+//? email
+//? password
+const { findUserByEmail } = require('../users/users.controllers');
+const { comparePassword } = require('../utils/crypto')
+
+//? Esta funcion va a validar si los datos pertenecen o no a un usuario
+const checkUserCredential = async (email, password) => {
+    try {
+        const user = await findUserByEmail(email)
+        const verifyPassword = comparePassword(password, user.password)
+        if(verifyPassword){
+            return user
+        }
+        return null
+    } catch (error) {
+        return null
+    }
+}
+
+
+module.exports = checkUserCredential
